cbfaacb850cc5edc55780323ea48387c0733a6c5
ProlificJuice <prolific_aus@hotmail.com>
2019-05-22 22:57:59 +1000

    PELASE

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '1005808556421-hc8hgp4blia93nvbktmv1t1efd826t7t.apps.googleusercontent.com',
+            clientSecret: 'IBVkhTn0MpLm_mYKq7SnpI-s'
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://prolific:prolific@prolific-nsump.mongodb.net/test?retryWrites=true'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
